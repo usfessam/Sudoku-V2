43e7931c6cd2c66eec1073ad5bcddf792cce91bc
eyadmotawe <eyadmotawe@gmail.com>
2025-11-30 19:05:08 +0200

    VerifierFactory.java

--- src/VerifierFactory.java
+++ src/VerifierFactory.java
@@ -1,3 +1,20 @@
 public class VerifierFactory {
+    public static Verifier getVerifier(int mode) {
+        
+        if (mode == 0) {
+            return new SequentialVerifier();
+        } 
+        else if (mode == 3) {
+             return new Mode3Verifier();
+        }
 
+        else if (mode == 27) {
+            return new Mode27Verifier();
+            
+        }
+        
+        return null;
+    }
 }
+    
+
