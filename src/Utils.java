88b176ae5c3115d19e428941d323fb051ff9dd47
‪Ziad AbdElhaleem‬‏ <ziadabdelhaleem63@gmail.com>
2025-12-01 02:21:08 +0200

    solve the warning

--- src/Utils.java
+++ src/Utils.java
@@ -4,23 +4,23 @@ import java.util.List;
 public class Utils {
 
     public static void analyzeSegment(int[] unit, String type, int index, List<String> errors) {
-        List<Integer>[] positions = new ArrayList[10];
-        
+        List<List<Integer>> positions = new ArrayList<>();
+
         for (int i = 0; i <= 9; i++) {
-            positions[i] = new ArrayList<>();
+            positions.add(new ArrayList<>());
         }
 
         for (int i = 0; i < 9; i++) {
             int num = unit[i];
             if (num >= 1 && num <= 9) {
-                positions[num].add(i + 1);
+                positions.get(num).add(i + 1);
             }
         }
 
         for (int num = 1; num <= 9; num++) {
-            if (positions[num].size() > 1) {
-                String errorMsg = String.format("%s %d, #%d, %s", 
-                                              type, (index + 1), num, positions[num].toString());
+            if (positions.get(num).size() > 1) {
+                String errorMsg = String.format("%s %d, #%d, %s",
+                        type, (index + 1), num, positions.get(num).toString());
                 errors.add(errorMsg);
             }
         }
